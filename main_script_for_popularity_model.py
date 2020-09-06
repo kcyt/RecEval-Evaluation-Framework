e2bbd6720e32ebe1ea16c31658933726f2cbed78
kcyt <chan.yanting.kennard@dhs.sg>
2020-09-07 00:03:27 +0800

    name changes

--- main_script_for_popularity_model.py
+++ main_script_for_popularity_model.py
@@ -1,10 +1,10 @@
-import pandas as pd
+import pandas as pd 
 from dataset import Deskdrop_Dataset
-from decay_popularity_model import DecayPopularityModel
+from popularity_model import PopularityModel
 from evaluator import Evaluator
 
 """
-An example on using this evaluation framework: You can replace 'deskdrop_dataset' and 'decay_popularity_model' with your own dataset and model objects. 
+An example on using this evaluation framework: You can replace 'deskdrop_dataset' and 'popularity_model' with your own dataset and model objects. 
 But your dataset object must be a subclass of Dataset defined in dataset.py and implement its abstract methods; 
 and your model object must be a subclass of Model defined in model.py and implement its abstract methods.
 """
@@ -16,24 +16,24 @@ n_ranks=[1, 3, 5] # For defining the metrics e.g. if n_ranks = [1,5], and the me
 
 # initialising the objects
 deskdrop_dataset = Deskdrop_Dataset(partition_ratios = [0.6, 0.2, 0.2], folder_path= 'CI&T_Deskdrop_dataset') 
-decay_popularity_model = DecayPopularityModel(n=num_of_recommendations, time_delay_from_t0 = 60,  num_of_interval = 6 )
+popularity_model = PopularityModel(n=num_of_recommendations, popularity_metric = "simple interaction count")
 evaluator = Evaluator(n_ranks=n_ranks)
 
 # do Training 
 training_set = deskdrop_dataset.getTraining()
-decay_popularity_model.train_in_batch(dataset=training_set)
+popularity_model.train_in_batch(dataset=training_set)
 
-# Validation in stream environment 
+# do Validation
 deskdrop_dataset.set_curr_pointer(mode='Validation')
-validation_results = evaluator.evaluate_model_in_stream( dataset=deskdrop_dataset, model=decay_popularity_model, mode='Validation', scheme = [0.9,0.1])
-
-# do Training with both Training and Validation sets:
 validation_set = deskdrop_dataset.getValidation(exclude_first_time_users = False)
+recommendation_df = popularity_model.predict_in_batch(validation_set=validation_set)
+validation_results = evaluator.evaluate_validation_set_in_batch(recommendation_df, validation_set )
+
+# Training with both Training and Validation sets:
 train_and_validate_sets = pd.concat([training_set, validation_set])
-decay_popularity_model.train_in_batch(dataset=train_and_validate_sets)
+popularity_model.train_in_batch(dataset=train_and_validate_sets)
 
 # evaluate your model
 deskdrop_dataset.set_curr_pointer(mode='Testing')
-testing_results = evaluator.evaluate_model_in_stream( dataset=deskdrop_dataset, model=decay_popularity_model, mode='Testing', scheme = [0.9,0.1])
-
+evaluation_results = evaluator.evaluate_model_in_stream( dataset=deskdrop_dataset, model=popularity_model, mode='Testing', scheme = [0.9,0.1])
 
